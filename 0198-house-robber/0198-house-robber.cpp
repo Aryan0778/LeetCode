6a4234729e707ac6d026e28007773b8044e8b7d0
Aryan0778 <132290602+Aryan0778@users.noreply.github.com>
2024-10-16 12:05:39 +0530

    Time: 0 ms (100%), Space: 9.6 MB (11.39%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
-    int maxm(vector<int>&nums,vector<int>&dp,int ind){
-        if(ind<0) return 0;
-        if(ind==0) return nums[0];
-        if(dp[ind]!=-1) return dp[ind];
-        int l=maxm(nums,dp,ind-1);
-        int r=nums[ind]+maxm(nums,dp,ind-2);
-        return dp[ind]=max(l,r);
-    }
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(n+1,0);
-        dp[0]=0;
-        dp[1]=nums[0];
-        for(int i=2;i<=n;i++){
-            int l=dp[i-1];
-            int r=dp[i-2]+nums[i-1];
-            dp[i]=max(l,r);
-        }
-        return dp[n];
+    int n=nums.size();
+    if(n==1) return nums[0];
+    vector<int>dp(n+1,0);
+    dp[0]=0;
+    dp[1]=nums[0];
+    for(int i=2;i<=n;i++){
+        int pick=nums[i-1]+dp[i-2];
+        int notpick=dp[i-1];
+        dp[i]=max(pick,notpick);
+    }
+    return dp[n];  
     }
 };
