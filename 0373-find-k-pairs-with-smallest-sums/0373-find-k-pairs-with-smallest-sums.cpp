9776bc266a7f4418b7c34b1a9a4db76f459025bb
Aryan0778 <132290602+Aryan0778@users.noreply.github.com>
2025-04-09 11:57:02 +0530

    Time: 95 ms (27.21%), Space: 135.6 MB (75.52%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
+    typedef pair<int,pair<int,int>> P;
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
     int m=nums1.size(),n=nums2.size();
-    priority_queue<pair<int,pair<int,int>>>maxHeap;
+    priority_queue<P>pq;
+    vector<vector<int>>ans;
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
-            int sum=nums1[i]+nums2[j];
-            if(maxHeap.size()<k){
-                maxHeap.push({sum,{nums1[i],nums2[j]}});
+            if(pq.size()<k){
+                pq.push({nums1[i]+nums2[j],{i,j}});
             }
-            else if(maxHeap.top().first>sum){
-                maxHeap.pop();
-                maxHeap.push({sum,{nums1[i],nums2[j]}});
+            else if(pq.top().first>nums1[i]+nums2[j]){
+                pq.pop();
+                pq.push({nums1[i]+nums2[j],{i,j}});   
             }
             else{
                 break;
             }
         }
     }
-    vector<vector<int>>ans;
-    while(maxHeap.size()>0){
-      ans.push_back({maxHeap.top().second.first,maxHeap.top().second.second});
-      maxHeap.pop();
+    while(pq.size()>0){
+        ans.push_back({nums1[pq.top().second.first],nums2[pq.top().second.second]});
+        pq.pop();
     }
-    return ans;   
+    return ans;
     }
 };
