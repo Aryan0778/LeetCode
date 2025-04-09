9b1f0f2b458a1480ef0b64cf890a9e286b9f434d
Aryan0778 <132290602+Aryan0778@users.noreply.github.com>
2025-04-09 12:34:23 +0530

    Time: 217 ms (7.86%), Space: 140.7 MB (62.97%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -2,27 +2,28 @@ class Solution {
 public:
     typedef pair<int,pair<int,int>> P;
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+    priority_queue<P,vector<P>,greater<P>>minHeap;
     int m=nums1.size(),n=nums2.size();
-    priority_queue<P>pq;
+    int sum=nums1[0]+nums2[0];
+    minHeap.push({sum,{0,0}});
     vector<vector<int>>ans;
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(pq.size()<k){
-                pq.push({nums1[i]+nums2[j],{i,j}});
-            }
-            else if(pq.top().first>nums1[i]+nums2[j]){
-                pq.pop();
-                pq.push({nums1[i]+nums2[j],{i,j}});   
-            }
-            else{
-                break;
-            }
+    set<pair<int,int>>visited;
+    visited.insert({0,0});
+    while(k-- && !minHeap.empty()){
+        auto temp=minHeap.top();
+        minHeap.pop();
+        int i=temp.second.first;
+        int j=temp.second.second;
+        ans.push_back({nums1[i],nums2[j]});
+        if(j+1<n && visited.find({i,j+1})==visited.end()){//Push (i,j+1) if possible
+        visited.insert({i,j+1});
+        minHeap.push({nums1[i]+nums2[j+1],{i,j+1}});
+        }
+        if(i+1<m && visited.find({i+1,j})==visited.end()){//Push (i+1,j) if possible
+        visited.insert({i+1,j});
+        minHeap.push({nums1[i+1]+nums2[j],{i+1,j}});
         }
     }
-    while(pq.size()>0){
-        ans.push_back({nums1[pq.top().second.first],nums2[pq.top().second.second]});
-        pq.pop();
-    }
-    return ans;
+    return ans;    
     }
 };
