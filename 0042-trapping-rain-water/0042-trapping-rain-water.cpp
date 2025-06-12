2277940da65e0dfd9e406822134d870e44425e23
Aryan0778 <132290602+Aryan0778@users.noreply.github.com>
2025-06-12 14:46:27 +0530

    Time: 176 ms (5.22%), Space: 27 MB (42.01%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int trap(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>pre(n,0),suff(n,0);
-        pre[0]=nums[0];
-        suff[n-1]=nums[n-1];
-        for(int i=1;i<n;i++){
-            pre[i]=max(pre[i-1],nums[i]);
-        }
-        for(int i=n-2;i>=0;i--){
-            suff[i]=max(suff[i+1],nums[i]);
-        }
-        int ans=0;
+        int n=nums.size(),ans=0;
+        stack<int>st;
         for(int i=0;i<n;i++){
-            ans+=min(pre[i],suff[i])-nums[i];
+            while(!st.empty() && nums[st.top()]<nums[i]){
+                int mid=st.top();
+                st.pop();
+                if(st.empty()) break;
+                int left=st.top();
+                int minHeight=min(nums[i]-nums[mid],nums[left]-nums[mid]);
+                int width=i-left-1;
+                ans+=(minHeight*width);
+            }
+            st.push(i);
+            cout<<ans<<endl;
         }
         return ans;
     }
