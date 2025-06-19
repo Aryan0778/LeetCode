38b210da90065847528035307962f1325a601f93
Aryan0778 <132290602+Aryan0778@users.noreply.github.com>
2025-06-19 14:33:01 +0530

    Time: 890 ms (14.76%), Space: 11.8 MB (25.3%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -10,16 +10,20 @@ public:
     }
     return false;    
     }
-    bool solve(int i,int j,int idx,int m,int n,vector<vector<char>>& board,string word,vector<vector<int>>hash){
+    bool solve(int i,int j,int idx,int m,int n,vector<vector<char>>& board,string word,vector<vector<int>>&hash){
       if(idx>=word.size()) return true;
       if(i>=m || j>=n || i<0 || j<0 || word[idx]!=board[i][j] || hash[i][j]==0) return false;
       hash[i][j]=0;
       bool a=solve(i,j-1,idx+1,m,n,board,word,hash); //left
+      if(a==true) return true;
       bool b=solve(i,j+1,idx+1,m,n,board,word,hash); //right
+      if(b==true) return true;
       bool c=solve(i-1,j,idx+1,m,n,board,word,hash); //up
+      if(c==true) return true;
       bool d=solve(i+1,j,idx+1,m,n,board,word,hash); //down
+      if(d==true) return true;
       hash[i][j]=1;
 
-      return a || b || c || d;
+      return false;
     }
 };
