7d96b6e949bbbbeb8dba93c527ed3702775d47c8
Aryan0778 <132290602+Aryan0778@users.noreply.github.com>
2025-06-19 13:50:49 +0530

    Time: 8 ms (11.16%), Space: 18.6 MB (10.42%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -10,10 +10,6 @@ public:
     }
 
     void gen(int ind,int target,vector<int>nums,vector<int>ds,vector<vector<int>>&ans){
-      if(ind>=nums.size()){
-        if(target==0) ans.push_back(ds);
-        return;
-      }
       if(target==0){
         ans.push_back(ds);
         return;
