f24dca5d106ce93eb0b43733d616147a289e693b
Aryan0778 <132290602+Aryan0778@users.noreply.github.com>
2025-06-26 15:18:05 +0530

    Time: 104 ms (9.09%), Space: 23.3 MB (8.51%) - LeetHub

--- 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.cpp
+++ 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.cpp
@@ -1,56 +1,44 @@
 class Solution {
 public:
-    bool isPossible(const string& s, int m, int k) {
-        int n = s.size();
-        int a = k, b = k, c = k;
-        int tail = n - m;
-
-        // Initial window: last m characters
-        for (int i = n - m; i < n; ++i) {
-            if (s[i] == 'a') a--;
-            else if (s[i] == 'b') b--;
-            else c--;
-        }
-
-        if (a <= 0 && b <= 0 && c <= 0) return true;
-
-        for (int i = n; i < n + m; ++i) {
-            // Add new character from the front (circularly)
-            if (s[i % n] == 'a') a--;
-            else if (s[i % n] == 'b') b--;
-            else c--;
-
-            // Remove old character from tail
-            if (s[tail % n] == 'a') a++;
-            else if (s[tail % n] == 'b') b++;
-            else c++;
-            tail++;
-
-            if (a <= 0 && b <= 0 && c <= 0) return true;
-        }
-
-        return false;
+    bool isPossible(string s,int m,int k){
+      int n=s.size();
+      int a=k,b=k,c=k,tail=n-m;
+      for(int i=n-m;i<n;i++){
+        if(s[i]=='a')a--;
+        else if(s[i]=='b') b--;
+        else c--;
+      }
+      if(a<=0 && b<=0 && c<=0) return true;
+      for(int i=n;i<n+m;i++){
+        if(s[i%n]=='a')a--;
+        else if(s[i%n]=='b') b--;
+        else c--;
+        if(s[tail%n]=='a')a++;
+        else if(s[tail%n]=='b') b++;
+        else c++;
+        tail++;
+        if(a<=0 && b<=0 && c<=0) return true;
+      }
+      return false;
     }
-
     int takeCharacters(string str, int k) {
-        int n = str.size();
-
-        // ✅ Check if it is even possible to take k of each character
+      int n=str.size();
+       // ✅ Check if it is even possible to take k of each character
         int ca = count(str.begin(), str.end(), 'a');
         int cb = count(str.begin(), str.end(), 'b');
         int cc = count(str.begin(), str.end(), 'c');
         if (ca < k || cb < k || cc < k) return -1;
-
-        int s = 0, e = n, ans = -1;
-        while (s <= e) {
-            int m = s + (e - s) / 2;
-            if (isPossible(str, m, k)) {
-                ans = m;
-                e = m - 1;
-            } else {
-                s = m + 1;
-            }
+      int s=0,e=n,ans=-1;
+      while(s<=e){
+        int m=s+(e-s)/2;
+        if(isPossible(str,m,k)){
+          ans=m;
+          e=m-1;
         }
-        return ans;
+        else{
+          s=m+1;
+        }
+      }
+      return ans;  
     }
 };
