ece6abfec7700a396f9f7134be4b46722dcfdc6b
Aryan0778 <132290602+Aryan0778@users.noreply.github.com>
2025-06-26 11:12:43 +0530

    Time: 1517 ms (5.29%), Space: 288.2 MB (5.29%) - LeetHub

--- 3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i/3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i.cpp
+++ 3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i/3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
-    bool isValid(vector<int>pre,vector<int>count){
-      for(int i=0;i<26;i++){
-        if(pre[i]<count[i]) return false;
-      }
-      return true;
-    }
     long long validSubstringCount(string s, string target) {
-        long long n=s.size(),m=target.size(),a=0,ans=0;
-        vector<int>count(26,0),pre(26,0);
-        for(auto it:target) count[it-'a']++;
-        for(int i=0;i<n;i++){
-          pre[s[i]-'a']++;
-          while(a<=i && isValid(pre,count)){
-            ans+=(n-i);
-            pre[s[a]-'a']--;
-            a++;
+    long long n=s.size();
+    vector<vector<long long>>pre(n+1,vector<long long>(26));
+    for(long long i=0;i<n;i++){
+      pre[i+1]=pre[i];
+      pre[i+1][s[i]-'a']++;
+    }
+    vector<int>hash(26,0);
+    for(auto it:target) hash[it-'a']++;
+
+    auto isCnt = [&] (int start,int end,int idx){
+      return pre[end+1][idx] - pre[start][idx];
+    };
+
+    auto findEnd= [&] (long long start){
+      long long s=start , e = n-1, ans=n;
+      while(s<=e){
+        long long mid=s+(e-s)/2;
+        bool isGood=true;
+        for(int i=0;i<26;i++){
+          if(isCnt(start,mid,i)<hash[i]){
+            isGood=false;
+            break;
           }
         }
-        return ans;
+        if(isGood) e=mid-1,ans=mid;
+        else s=mid+1;
+      }
+      return ans;
+    };
+    long long cnt=0;
+    for(int i=0;i<n;i++){
+      int e = findEnd(i);
+      cnt+=(n-e);
+    }
+    return cnt;   
     }
 };
